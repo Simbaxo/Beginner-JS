3a9b9bddac96d89d6e349c6af5f74848c04dc58d
Andres Mena <ballerlikemike23@gmail.com>
2020-01-02 19:23:36 -0500

    Functions - Parameters and Arguments

--- 01 - Basics/custom-functions/cf.js
+++ 01 - Basics/custom-functions/cf.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-console */
 // Function Definition
-const bill = 100
-const taxRate = 0.13
-
-function calculateBill() {
+function calculateBill(billAmount, taxRate) {
+  console.log(billAmount, taxRate)
   // this is the function body
   console.log('Running Calculate Bill')
-  const total = bill * 1 + taxRate
+  const total = billAmount * (1 + taxRate)
   return total
 }
 
 // Function Call
-const myTotal = calculateBill()
-consty myTotal2 = calculateBill()
-console.log(myTotal, myTotal2)
+const myTotal = calculateBill(100, 0.13)
+const myTotal2 = calculateBill(200, 0.13)
+// console.log(myTotal, myTotal2)
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`
+}
+
+// const greeting = sayHiTo('Dre')
+// console.log(greeting)
+
+// const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15)
+
+function doctorize(name) {
+  return `Dr. ${name}`
+}
+
+function yell(name) {
+  return `HEY ${name.toUpperCase()}`
+}
+yell(doctorize('dre'))
